d557f5bd6eb708ef72e156af8725e254684e4910
Arturs-Ilva <ilvaarturs@inbox.lv>
2023-01-31 09:08:30 +0200

    Parveidoju textboxus un tekstu, izveidoju kodu

--- ai_Saldumi_11/Form1.Designer.cs
+++ ai_Saldumi_11/Form1.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.Saldumi_aprekinat_button = new System.Windows.Forms.Button();
-            this.Saldumi_daudzums_textbox = new System.Windows.Forms.TextBox();
+            this.Nauda_daudzums_textbox = new System.Windows.Forms.TextBox();
             this.Saldumi_rezultats_textbox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Saldumi_aprekinat_button
@@ -46,13 +48,13 @@
             this.Saldumi_aprekinat_button.UseVisualStyleBackColor = true;
             this.Saldumi_aprekinat_button.Click += new System.EventHandler(this.Saldumi_aprekinat_button_Click);
             // 
-            // Saldumi_daudzums_textbox
+            // Nauda_daudzums_textbox
             // 
-            this.Saldumi_daudzums_textbox.Location = new System.Drawing.Point(145, 215);
-            this.Saldumi_daudzums_textbox.Name = "Saldumi_daudzums_textbox";
-            this.Saldumi_daudzums_textbox.Size = new System.Drawing.Size(96, 20);
-            this.Saldumi_daudzums_textbox.TabIndex = 1;
-            this.Saldumi_daudzums_textbox.TextChanged += new System.EventHandler(this.Saldumi_daudzums_textbox_TextChanged);
+            this.Nauda_daudzums_textbox.Location = new System.Drawing.Point(145, 215);
+            this.Nauda_daudzums_textbox.Name = "Nauda_daudzums_textbox";
+            this.Nauda_daudzums_textbox.Size = new System.Drawing.Size(96, 20);
+            this.Nauda_daudzums_textbox.TabIndex = 1;
+            this.Nauda_daudzums_textbox.TextChanged += new System.EventHandler(this.Saldumi_daudzums_textbox_TextChanged);
             // 
             // Saldumi_rezultats_textbox
             // 
@@ -64,11 +66,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(481, 199);
+            this.label1.Location = new System.Drawing.Point(471, 199);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.Size = new System.Drawing.Size(106, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Rezultats/Cena";
+            this.label1.Text = "Rezultats/Daudzums";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
@@ -76,30 +78,50 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(77, 50);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(637, 13);
+            this.label2.Size = new System.Drawing.Size(704, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Sveicinati saldumu veikala \"Artura saldumi un vel kautkas\"! Seit jus aprekinasiet" +
-    " jusu saldumu cenu, musu saldumi maksa 9.88eur/kg.";
+    " cik saldumus jus varat iegadaties, musu saldumi maksa 9,88eur/kg.";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(145, 198);
+            this.label3.Location = new System.Drawing.Point(143, 199);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(161, 13);
+            this.label3.Size = new System.Drawing.Size(93, 13);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Cik kg jus nemsiet resnais cuka?";
+            this.label3.Text = "Cik jums ir nauda?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(80, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(276, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Lugums ievadot skaitli ar decimaldalam, izmantot komatu!";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(80, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(158, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "*Decimaldala - Cipari aiz komata";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Saldumi_rezultats_textbox);
-            this.Controls.Add(this.Saldumi_daudzums_textbox);
+            this.Controls.Add(this.Nauda_daudzums_textbox);
             this.Controls.Add(this.Saldumi_aprekinat_button);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -111,11 +133,13 @@
         #endregion
 
         private System.Windows.Forms.Button Saldumi_aprekinat_button;
-        private System.Windows.Forms.TextBox Saldumi_daudzums_textbox;
+        private System.Windows.Forms.TextBox Nauda_daudzums_textbox;
         private System.Windows.Forms.TextBox Saldumi_rezultats_textbox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
